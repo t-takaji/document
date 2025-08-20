845cc16a2485ed55dad00dcfd6404bb879d43ba5
t-takaji <42364759+t-takaji@users.noreply.github.com>
2025-08-20 09:58:27 +0900

    Delete Docker/20220217_search directory

--- Docker/20220217_search/config/mock.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'search' => [json_decode(file_get_contents(__DIR__ . '/json/search.json'), true)], 
-    'getschema' => [json_decode(file_get_contents(__DIR__ . '/json/getschema.json'), true)
-    ]
-];
